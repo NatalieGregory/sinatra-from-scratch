44366a4194e8f1dad5e02d214a8e90be3c842ca9
Natalie Gregory <gregorynr@gmail.com>
2014-07-16 09:56:27 -0600

    warmup 2014-07-16

---
+++ spec/myapp_spec.rb
@@ -0,0 +1,10 @@
+require "spec_helper"
+
+describe Myapp do
+  it "has a cool name" do
+    expect(page).to include_content("My Cool App")
+  end
+
+  it "allows people to signup vie email"
+end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require "sinatra"
+require "rspec"
+require "capybara"
+require "myapp_dbase"
+
+Capybara.app = MyApp
